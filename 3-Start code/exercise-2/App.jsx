aafdd18f706bc19627a692bafc4e047873fa8bf7
Sreyoun12 <sreyoun.channy@student.cadt.edu.kh>
2026-02-09 20:45:56 +0700

    Week4-assignment

---
+++ 3-Start code/exercise-2/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx"; 
+import Main from "./Main.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
